59916c896f2df17dae440f42654dfd06072160cc
sidthekid-11 <rakheesiddhant2013@gmail.com>
2025-12-03 16:31:47 +0530

    initial commit

--- swapping3numbers.py
+++ swapping3numbers.py
@@ -1,10 +1,15 @@
-a=66 
-b=77
-c=34
+a=int(input("Please enter your number:"))
+b=int(input("Please enter your number:"))
+c=int(input("Please enter your number:"))
 
 a=a+b+c
-b=a-b+c
-c=a+b-c
+#print(a)
+b=a-(b+c)
+#print(b)
+c=a-(b+c)
+#print(c)
+a=a-(b+c)
+#print(a)
 
 print(a)
 print(b)
